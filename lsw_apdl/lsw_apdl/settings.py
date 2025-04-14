7d5c91375b36df6645d7c8800c40583c89ee49f8
EdwardMase <105016548+Doneddie@users.noreply.github.com>
2025-04-14 06:12:26 +0000

    Ready for deployment

--- lsw_apdl/lsw_apdl/settings.py
+++ lsw_apdl/lsw_apdl/settings.py
@@ -32,9 +32,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "your-default-dev-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Doneddie.pythonanywhere.com']
 CSRF_TRUSTED_ORIGINS = [
     "https://localhost:8000",
 ]
@@ -153,6 +153,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
